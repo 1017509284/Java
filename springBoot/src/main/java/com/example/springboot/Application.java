d74d14e10b5557cef51d4ce743feaea82161a823
赵萌 <zhaomeng@zhaomenndiannao.lan>
2022-12-13 22:15:05 +0800

    添加注释

--- springBoot/src/main/java/com/example/springboot/Application.java
+++ springBoot/src/main/java/com/example/springboot/Application.java
@@ -3,10 +3,12 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// !标注这是一个springboot的应用
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // !将springboot应用启动
         SpringApplication.run(Application.class, args);
     }
 
